8a2c114b7efc5666a5f539d064f4f33496b3304e
Asaad164 <157280290+Asaad164@users.noreply.github.com>
2025-04-09 10:59:01 +0300

    Add files via upload

---
+++ FinalProjSer/M.py
@@ -0,0 +1,45 @@
+import pandas as pd
+from firebase import firebase
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+
+
+FBconn = firebase.FirebaseApplication('https://finalprojdb-40136-default-rtdb.europe-west1.firebasedatabase.app/', None)
+
+
+data_k = FBconn.get('/AllK', None)
+data_h = FBconn.get('/AllH', None)
+data_n = FBconn.get('/AllN', None)
+
+
+df_k = pd.DataFrame(data_k)
+df_h = pd.DataFrame(data_h)
+df_n = pd.DataFrame(data_n)
+
+# Combine
+df = pd.concat([df_k, df_h, df_n], ignore_index=True)
+
+df = df.dropna()
+
+df['Date of sale'] = pd.to_datetime(df['Date of sale'], errors='coerce')
+df['SaleYear'] = df['Date of sale'].dt.year
+df['SaleMonth'] = df['Date of sale'].dt.month
+df = df.drop(columns=['Date of sale'])
+
+
+df = pd.get_dummies(df, columns=['Type','City'])
+
+df['Block'] = df['Block'].astype(str).str[:-1].astype(int)
+#df['old'] = df['SaleYear'].astype(int) - df['Year Built'].astype(int)
+X = df.drop(columns=['Selling price in NIS'])
+y = df['Selling price in NIS']
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestRegressor(random_state=42)
+model.fit(X_train, y_train)
+
+
+score = model.score(X_test, y_test)
+print(f"Model R² Score: {score:.2f}")
